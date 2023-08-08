2ddc92908f8ffa7b382969337154dfb987cc8c0d
vikram18rock <v16ash@gmail.com>
2023-08-08 18:25:18 +0530

    exercise 1 implemented

--- mynitcbase/main.cpp
+++ mynitcbase/main.cpp
@@ -3,9 +3,17 @@
 #include "Disk_Class/Disk.h"
 #include "FrontendInterface/FrontendInterface.h"
 
+#include <cstring>
+#include <iostream>
+using namespace std;
 int main(int argc, char *argv[]) {
-  /* Initialize the Run Copy of Disk */
   Disk disk_run;
 
-  return FrontendInterface::handleFrontend(argc, argv);
+  unsigned char buffer[BLOCK_SIZE];
+  Disk::readBlock(buffer, 0);
+  for(int i = 0; i < 6; i++) {
+    cout << (int)buffer[i] << ",";
+  }
+  cout << endl;
+  return 0;
 }
