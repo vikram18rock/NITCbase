e285f76f6e3599f0b6a45162600304fa35b0dda5
vikram18rock <v16ash@gmail.com>
2023-08-10 16:53:00 +0530

    Summary: Added a new function to the `updateAttr()` function.
    Description: Divided the codes into funcions to use schema again. The new function allows users to update the names of attributes in a relation. Still need to update the function to write back into attribute catalog
    
    Files: updateAttr.cpp

--- mynitcbase/main.cpp
+++ mynitcbase/main.cpp
@@ -5,9 +5,9 @@
 
 #include <cstring>
 #include <iostream>
+using namespace std;
 
-int main(int argc, char *argv[]) {
-  Disk disk_run;
+void schema() {
 
   // create objects for the relation catalog and attribute catalog
   RecBuffer relCatBuffer(RELCAT_BLOCK);
@@ -52,6 +52,57 @@ int main(int argc, char *argv[]) {
     }
     printf("\n");
   }
+}
 
+void updateAttr(char relName[], char oldAttr[], char updAttr[]) {
+
+  // create objects for the relation catalog and attribute catalog
+  RecBuffer relCatBuffer(RELCAT_BLOCK);
+  RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
+
+  HeadInfo relCatHeader;
+  HeadInfo attrCatHeader;
+
+  // load the headers of both the blocks into relCatHeader and attrCatHeader.
+  // (we will implement these functions later)
+  relCatBuffer.getHeader(&relCatHeader);
+  attrCatBuffer.getHeader(&attrCatHeader);
+
+  for (int i = 0, noOfAttrs = 0; i < relCatHeader.numEntries; i++) {
+ 
+    Attribute relCatRecord[RELCAT_NO_ATTRS]; // will store the record from the relation catalog
+
+    relCatBuffer.getRecord(relCatRecord, i);
+
+
+    for (int j = 0; j < relCatRecord[RELCAT_NO_ATTRIBUTES_INDEX].nVal; j++, noOfAttrs++) {
+
+      // declare attrCatRecord and load the attribute catalog entry into it
+      Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+      attrCatBuffer.getRecord(attrCatRecord, noOfAttrs);
+
+      /* attribute catalog entry corresponds to the current relation */
+      if (strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal, relCatRecord[RELCAT_REL_NAME_INDEX].sVal) == 0 && strcmp(attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, oldAttr)) {
+        strcpy(attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, updAttr);
+      }
+      if(noOfAttrs == attrCatHeader.numSlots - 1) {
+        // re initializing the slot index
+        noOfAttrs = -1;
+        // updating the buffer block with next block;
+        attrCatBuffer = RecBuffer(attrCatHeader.rblock);
+        // updating the header
+        attrCatBuffer.getHeader(&attrCatHeader);
+      }
+    }
+    printf("\n");
+  }
+}
+
+int main(int argc, char *argv[]) {
+  Disk disk_run;
+  char rel_name[16] = "Students", old_attr[16] = "Class", upd_attr[16] = "Batch";
+  schema();
+  updateAttr(rel_name, old_attr, upd_attr);
+  schema();
   return 0;
 }
