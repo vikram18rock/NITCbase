a10d88f561e87da987cbcf33516399efde3a931a
vikram18rock <v16ash@gmail.com>
2023-08-12 21:45:57 +0530

    Added 'update attribute' function in main, 'setrecord' function to write back the block to disk working good till finding the correct attribute but failing to update to disk. modified:   Buffer/BlockBuffer.cpp modified:   main.cpp

--- mynitcbase/Buffer/BlockBuffer.cpp
+++ mynitcbase/Buffer/BlockBuffer.cpp
@@ -54,3 +54,33 @@ int RecBuffer::getRecord(union Attribute *rec, int slotNum) {
 
   return SUCCESS;
 }
+
+// load the record at slotNum into the argument pointer
+int RecBuffer::setRecord(union Attribute *rec, int slotNum) {
+  struct HeadInfo head;
+
+  // get the header using this.getHeader() function
+  this->getHeader(&head);
+
+  int attrCount = head.numAttrs;
+  int slotCount = head.numSlots;
+
+  // read the block at this.blockNum into a buffer
+  unsigned char buffer[BLOCK_SIZE];
+  Disk::readBlock(buffer, this->blockNum);
+
+  /* record at slotNum will be at offset HEADER_SIZE + slotMapSize + (recordSize * slotNum)
+     - each record will have size attrCount * ATTR_SIZE
+     - slotMap will be of size slotCount
+  */
+  int recordSize = attrCount * ATTR_SIZE;
+  unsigned char *slotPointer = buffer + HEADER_SIZE + slotCount + (recordSize * slotNum);
+
+  // load the record from rec into the slotpointer
+  memcpy(slotPointer, rec, recordSize);
+
+  // write back into disk now
+  Disk::writeBlock(buffer, this->blockNum);
+
+  return SUCCESS;
+}
--- mynitcbase/main.cpp
+++ mynitcbase/main.cpp
@@ -56,45 +56,35 @@ void schema() {
 
 void updateAttr(char relName[], char oldAttr[], char updAttr[]) {
 
-  // create objects for the relation catalog and attribute catalog
-  RecBuffer relCatBuffer(RELCAT_BLOCK);
+  // get attribute catalogue block and it's header
   RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
-
-  HeadInfo relCatHeader;
   HeadInfo attrCatHeader;
 
-  // load the headers of both the blocks into relCatHeader and attrCatHeader.
-  // (we will implement these functions later)
-  relCatBuffer.getHeader(&relCatHeader);
   attrCatBuffer.getHeader(&attrCatHeader);
 
-  for (int i = 0, noOfAttrs = 0; i < relCatHeader.numEntries; i++) {
- 
-    Attribute relCatRecord[RELCAT_NO_ATTRS]; // will store the record from the relation catalog
+  // search through all attribute records
+  for(int i = 0; i < attrCatHeader.numEntries; i++) {
+    // declare attrCatRecord
+    Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
 
-    relCatBuffer.getRecord(relCatRecord, i);
+    // now get the records one by one
+    attrCatBuffer.getRecord(attrCatRecord, i);
 
-
-    for (int j = 0; j < relCatRecord[RELCAT_NO_ATTRIBUTES_INDEX].nVal; j++, noOfAttrs++) {
-
-      // declare attrCatRecord and load the attribute catalog entry into it
-      Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
-      attrCatBuffer.getRecord(attrCatRecord, noOfAttrs);
-
-      /* attribute catalog entry corresponds to the current relation */
-      if (strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal, relCatRecord[RELCAT_REL_NAME_INDEX].sVal) == 0 && strcmp(attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, oldAttr)) {
-        strcpy(attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, updAttr);
-      }
-      if(noOfAttrs == attrCatHeader.numSlots - 1) {
-        // re initializing the slot index
-        noOfAttrs = -1;
-        // updating the buffer block with next block;
-        attrCatBuffer = RecBuffer(attrCatHeader.rblock);
-        // updating the header
-        attrCatBuffer.getHeader(&attrCatHeader);
-      }
+    /* attribute catalog entry corresponds to the current relation */
+    if (strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal, relName) == 0 && strcmp(attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, oldAttr)) {
+      strcpy(attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, updAttr);
+      attrCatBuffer.setRecord(attrCatRecord, i);
+      std::cout << "Updated\n"; 
+      break;
     }
-    printf("\n");
+    if(i == attrCatHeader.numSlots - 1) {
+      // re initializing the slot index
+      i = -1;
+      // updating the buffer block with next block;
+      attrCatBuffer = RecBuffer(attrCatHeader.rblock);
+      // updating the header
+      attrCatBuffer.getHeader(&attrCatHeader);
+    }
   }
 }
 
