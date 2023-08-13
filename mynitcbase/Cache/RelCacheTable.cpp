4d99cac65b68fa348602581b9ad610159bb95302
vikram18rock <v16ash@gmail.com>
2023-08-13 10:48:21 +0530

    Filled the blanks in codes for modified files modified:   Cache/AttrCacheTable.cpp modified:   Cache/RelCacheTable.cpp

--- mynitcbase/Cache/RelCacheTable.cpp
+++ mynitcbase/Cache/RelCacheTable.cpp
@@ -29,8 +29,7 @@ int RelCacheTable::getRelCatEntry(int relId, RelCatEntry* relCatBuf) {
     This function will convert that to a struct RelCatEntry type.
 NOTE: this function expects the caller to allocate memory for `*relCatEntry`
 */
-void RelCacheTable::recordToRelCatEntry(union Attribute record[RELCAT_NO_ATTRS],
-                                        RelCatEntry* relCatEntry) {
+void RelCacheTable::recordToRelCatEntry(union Attribute record[RELCAT_NO_ATTRS], RelCatEntry* relCatEntry) {
   strcpy(relCatEntry->relName, record[RELCAT_REL_NAME_INDEX].sVal);
   relCatEntry->numAttrs = (int)record[RELCAT_NO_ATTRIBUTES_INDEX].nVal;
 
@@ -40,4 +39,8 @@ void RelCacheTable::recordToRelCatEntry(union Attribute record[RELCAT_NO_ATTRS],
       RELCAT_LAST_BLOCK_INDEX,
       RELCAT_NO_SLOTS_PER_BLOCK_INDEX
   */
+  relCatEntry->numRecs = (int)record[RELCAT_NO_RECORDS_INDEX].nVal;
+  relCatEntry->firstBlk = (int)record[RELCAT_FIRST_BLOCK_INDEX].nVal;
+  relCatEntry->lastBlk = (int)record[RELCAT_LAST_BLOCK_INDEX].nVal;
+  relCatEntry->numSlotsPerBlk = (int)record[RELCAT_NO_SLOTS_PER_BLOCK_INDEX].nVal;
 }
