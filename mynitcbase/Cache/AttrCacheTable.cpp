4917dc95e7f78d021ea0709659b0d9dd280402b4
vikram18rock <v16ash@gmail.com>
2023-08-13 09:43:14 +0530

    Updated the files to cache section of stage 3, on completion of previous sections modified:   Cache/AttrCacheTable.cpp modified:   Cache/RelCacheTable.cpp modified:   main.cpp

--- mynitcbase/Cache/AttrCacheTable.cpp
+++ mynitcbase/Cache/AttrCacheTable.cpp
@@ -1,3 +1,36 @@
 #include "AttrCacheTable.h"
 
 #include <cstring>
+
+AttrCacheEntry* AttrCacheTable::attrCache[MAX_OPEN];
+
+/* returns the attrOffset-th attribute for the relation corresponding to relId
+NOTE: this function expects the caller to allocate memory for `*attrCatBuf`
+*/
+int AttrCacheTable::getAttrCatEntry(int relId, int attrOffset, AttrCatEntry* attrCatBuf) {
+  // check if 0 <= relId < MAX_OPEN and return E_OUTOFBOUND otherwise
+
+  // check if attrCache[relId] == nullptr and return E_RELNOTOPEN if true
+
+  // traverse the linked list of attribute cache entries
+  for (AttrCacheEntry* entry = attrCache[relId]; entry != nullptr; entry = entry->next) {
+    if (entry->attrCatEntry.offset == attrOffset) {
+
+      // copy entry->attrCatEntry to *attrCatBuf and return SUCCESS;
+    }
+  }
+
+  // there is no attribute at this offset
+  return E_ATTRNOTEXIST;
+}
+
+/* Converts a attribute catalog record to AttrCatEntry struct
+    We get the record as Attribute[] from the BlockBuffer.getRecord() function.
+    This function will convert that to a struct AttrCatEntry type.
+*/
+void AttrCacheTable::recordToAttrCatEntry(union Attribute record[ATTRCAT_NO_ATTRS],
+                                          AttrCatEntry* attrCatEntry) {
+  strcpy(attrCatEntry->relName, record[ATTRCAT_REL_NAME_INDEX].sVal);
+
+  // copy the rest of the fields in the record to the attrCacheEntry struct
+}
--- mynitcbase/main.cpp
+++ mynitcbase/main.cpp
@@ -9,6 +9,7 @@
 int main(int argc, char *argv[]) {
   Disk disk_run;
   StaticBuffer buffer;
+  OpenRelTable Cache;
 
   // create objects for the relation catalog and attribute catalog
   RecBuffer relCatBuffer(RELCAT_BLOCK);
