c468a2a51ce5bbeca971ceb86d0cdf8b24b1562a
vikram18rock <v16ash@gmail.com>
2023-08-14 18:33:21 +0530

    Updated the codes as guided by comments

--- mynitcbase/Cache/RelCacheTable.cpp
+++ mynitcbase/Cache/RelCacheTable.cpp
@@ -51,8 +51,14 @@ NOTE: this function expects the caller to allocate memory for `*searchIndex`
 */
 int RelCacheTable::getSearchIndex(int relId, RecId* searchIndex) {
   // check if 0 <= relId < MAX_OPEN and return E_OUTOFBOUND otherwise
+  if(relId >= MAX_OPEN || relId < 0) {
+    return E_OUTOFBOUND;
+  }
 
   // check if relCache[relId] == nullptr and return E_RELNOTOPEN if true
+  if(relCache[relId] == nullptr) {
+    return E_RELNOTOPEN;
+  }
 
   // copy the searchIndex field of the Relation Cache entry corresponding
   //   to input relId to the searchIndex variable.
@@ -64,14 +70,25 @@ int RelCacheTable::getSearchIndex(int relId, RecId* searchIndex) {
 int RelCacheTable::setSearchIndex(int relId, RecId* searchIndex) {
 
   // check if 0 <= relId < MAX_OPEN and return E_OUTOFBOUND otherwise
+  if(relId >= MAX_OPEN || relId < 0) {
+    return E_OUTOFBOUND;
+  }
 
   // check if relCache[relId] == nullptr and return E_RELNOTOPEN if true
+  if(relCache[relId] == nullptr) {
+    return E_RELNOTOPEN;
+  }
 
   // update the searchIndex value in the relCache for the relId to the searchIndex argument
+  relCache[relId]->searchIndex = *searchIndex;
 
   return SUCCESS;
 }
 
 int RelCacheTable::resetSearchIndex(int relId) {
   // use setSearchIndex to set the search index to {-1, -1}
+  RecId si;
+  si.block = -1;
+  si.slot = -1;
+  return setSearchIndex(relId, &si);
 }
