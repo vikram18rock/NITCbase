736f0123e4f693d30dc04025d98d7ded06b20bd4
vikram18rock <v16ash@gmail.com>
2023-08-14 19:27:47 +0530

    Updated the code as guided by comments modified:   Buffer/BlockBuffer.cpp modified:   Cache/AttrCacheTable.cpp

--- mynitcbase/Buffer/BlockBuffer.cpp
+++ mynitcbase/Buffer/BlockBuffer.cpp
@@ -15,8 +15,8 @@ RecBuffer::RecBuffer(int blockNum) : BlockBuffer::BlockBuffer(blockNum) {}
 Used to get the header of the block into the location pointed to by `head`
 NOTE: this function expects the caller to allocate memory for `head`
 */
-int BlockBuffer::getHeader(struct HeadInfo *head) {
-  unsigned char *bufferPtr;
+int BlockBuffer::getHeader(struct HeadInfo* head) {
+  unsigned char* bufferPtr;
   int ret = loadBlockAndGetBufferPtr(&bufferPtr);
   if (ret != SUCCESS) {
     return ret;   // return any errors that might have occured in the process
@@ -36,10 +36,10 @@ int BlockBuffer::getHeader(struct HeadInfo *head) {
 Used to get the record at slot `slotNum` into the array `rec`
 NOTE: this function expects the caller to allocate memory for `rec`
 */
-int RecBuffer::getRecord(union Attribute *rec, int slotNum) {
+int RecBuffer::getRecord(union Attribute* rec, int slotNum) {
 
   // loading the block at this->blockNum into buffer {used here, instead of Disk::readBlock()}
-  unsigned char *bufferPtr;
+  unsigned char* bufferPtr;
   int ret = loadBlockAndGetBufferPtr(&bufferPtr);
   if (ret != SUCCESS) {
     return ret;
@@ -58,7 +58,7 @@ int RecBuffer::getRecord(union Attribute *rec, int slotNum) {
      - slotMap will be of size slotCount
   */
   int recordSize = attrCount * ATTR_SIZE;
-  unsigned char *slotPointer = bufferPtr + HEADER_SIZE + slotCount + (recordSize * slotNum);
+  unsigned char* slotPointer = bufferPtr + HEADER_SIZE + slotCount + (recordSize * slotNum);
 
   // load the record into the rec data structure
   memcpy(rec, slotPointer, recordSize);
@@ -70,7 +70,7 @@ int RecBuffer::getRecord(union Attribute *rec, int slotNum) {
 Used to load a block to the buffer and get a pointer to it.
 NOTE: this function expects the caller to allocate memory for the argument
 */
-int BlockBuffer::loadBlockAndGetBufferPtr(unsigned char **buffPtr) {
+int BlockBuffer::loadBlockAndGetBufferPtr(unsigned char** buffPtr) {
   // check whether the block is already present in the buffer using StaticBuffer.getBufferNum()
   int bufferNum = StaticBuffer::getBufferNum(this->blockNum);
 
@@ -93,8 +93,8 @@ int BlockBuffer::loadBlockAndGetBufferPtr(unsigned char **buffPtr) {
 /* used to get the slotmap from a record block
 NOTE: this function expects the caller to allocate memory for `*slotMap`
 */
-int RecBuffer::getSlotMap(unsigned char *slotMap) {
-  unsigned char *bufferPtr;
+int RecBuffer::getSlotMap(unsigned char* slotMap) {
+  unsigned char* bufferPtr;
 
   // get the starting address of the buffer containing the block using loadBlockAndGetBufferPtr().
   int ret = loadBlockAndGetBufferPtr(&bufferPtr);
@@ -104,13 +104,51 @@ int RecBuffer::getSlotMap(unsigned char *slotMap) {
 
   struct HeadInfo head;
   // get the header of the block using getHeader() function
+  this->getHeader(&head);
 
-  int slotCount = /* number of slots in block from header */;
+  int slotCount = head.numSlots;
 
   // get a pointer to the beginning of the slotmap in memory by offsetting HEADER_SIZE
-  unsigned char *slotMapInBuffer = bufferPtr + HEADER_SIZE;
+  unsigned char* slotMapInBuffer = bufferPtr + HEADER_SIZE;
 
   // copy the values from `slotMapInBuffer` to `slotMap` (size is `slotCount`)
+  memcpy(slotMap, slotMapInBuffer, slotCount);
 
   return SUCCESS;
+}
+
+
+int compareAttrs(union Attribute attr1, union Attribute attr2, int attrType) {
+
+  double diff;
+
+  // if attrType == STRING
+  //     diff = strcmp(attr1.sval, attr2.sval)
+
+  // else
+  //     diff = attr1.nval - attr2.nval
+
+  if (attrType == STRING) {
+    diff = strcmp(attr1.sVal, attr2.sVal);
+  }
+  else {
+    diff = attr1.nVal - attr2.nVal;
+  }
+
+  /*
+  if diff > 0 then return 1
+  if diff < 0 then return -1
+  if diff = 0 then return 0
+  */
+
+  if (diff > 0) {
+    return 1;
+  }
+  else if (diff < 0) {
+    return -1;
+  }
+  else {
+    return 0;
+  }
+
 }
