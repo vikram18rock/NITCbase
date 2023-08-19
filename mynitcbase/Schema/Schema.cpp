a084cf2e294e8ffce52d45dcb8d31b1a4a11479d
vikram18rock <v16ash@gmail.com>
2023-08-19 18:49:34 +0530

    added the renameAttr() function modified:   Schema/Schema.cpp

--- mynitcbase/Schema/Schema.cpp
+++ mynitcbase/Schema/Schema.cpp
@@ -70,12 +70,25 @@ int Schema::renameAttr(char *relName, char *oldAttrName, char *newAttrName) {
         // return E_NOTPERMITTED
         // (check if the relation names are either "RELATIONCAT" and "ATTRIBUTECAT".
         // you may use the following constants: RELCAT_NAME and ATTRCAT_NAME)
+    if (
+      strcmp(newRelName, RELCAT_RELNAME) == 0 &&
+      strcmp(newRelName, ATTRCAT_RELNAME) == 0 &&
+      strcmp(oldRelName, RELCAT_RELNAME) == 0 &&
+      strcmp(oldRelName, ATTRCAT_RELNAME) == 0
+      ) {
+      return E_NOTPERMITTED;
+    }
 
     // if the relation is open
         //    (check if OpenRelTable::getRelId() returns E_RELNOTOPEN)
         //    return E_RELOPEN
-
+    int retVal = OpenRelTable::getRelId(oldRelName);
+    if (retVal == E_RELOPEN) {
+      return E_RELOPEN;
+    }
+    
     // Call BlockAccess::renameAttribute with appropriate arguments.
 
     // return the value returned by the above renameAttribute() call
+    return BlockAccess::renameAttribute(relName, oldAttrName, newAttrName);
 }
