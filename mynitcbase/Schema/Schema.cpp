76eccf8590eb00c563c6b5fe2524b6be3b392b7e
vikram18rock <v16ash@gmail.com>
2023-08-20 10:51:42 +0530

    Mistake in logic of Schema.cpp modified:   Schema/Schema.cpp

--- mynitcbase/Schema/Schema.cpp
+++ mynitcbase/Schema/Schema.cpp
@@ -41,9 +41,9 @@ int Schema::renameRel(char oldRelName[ATTR_SIZE], char newRelName[ATTR_SIZE]) {
       // (check if the relation names are either "RELATIONCAT" and "ATTRIBUTECAT".
       // you may use the following constants: RELCAT_NAME and ATTRCAT_NAME)
   if (
-    strcmp(newRelName, RELCAT_RELNAME) == 0 &&
-    strcmp(newRelName, ATTRCAT_RELNAME) == 0 &&
-    strcmp(oldRelName, RELCAT_RELNAME) == 0 &&
+    strcmp(newRelName, RELCAT_RELNAME) == 0 ||
+    strcmp(newRelName, ATTRCAT_RELNAME) == 0 ||
+    strcmp(oldRelName, RELCAT_RELNAME) == 0 ||
     strcmp(oldRelName, ATTRCAT_RELNAME) == 0
     ) {
     return E_NOTPERMITTED;
@@ -65,28 +65,28 @@ int Schema::renameRel(char oldRelName[ATTR_SIZE], char newRelName[ATTR_SIZE]) {
 
 /* This method changes the name of an attribute/column present in
  a specified relation, to new name as specified in arguments. */
-int Schema::renameAttr(char *relName, char *oldAttrName, char *newAttrName) {
-    // if the relName is either Relation Catalog or Attribute Catalog,
-        // return E_NOTPERMITTED
-        // (check if the relation names are either "RELATIONCAT" and "ATTRIBUTECAT".
-        // you may use the following constants: RELCAT_NAME and ATTRCAT_NAME)
-    if (
-      strcmp(relName, RELCAT_RELNAME) == 0 &&
-      strcmp(relName, ATTRCAT_RELNAME) == 0
-      ) {
-      return E_NOTPERMITTED;
-    }
+int Schema::renameAttr(char* relName, char* oldAttrName, char* newAttrName) {
+  // if the relName is either Relation Catalog or Attribute Catalog,
+      // return E_NOTPERMITTED
+      // (check if the relation names are either "RELATIONCAT" and "ATTRIBUTECAT".
+      // you may use the following constants: RELCAT_NAME and ATTRCAT_NAME)
+  if (
+    strcmp(relName, RELCAT_RELNAME) == 0 ||
+    strcmp(relName, ATTRCAT_RELNAME) == 0
+    ) {
+    return E_NOTPERMITTED;
+  }
 
-    // if the relation is open
-        //    (check if OpenRelTable::getRelId() returns E_RELNOTOPEN)
-        //    return E_RELOPEN
-    int retVal = OpenRelTable::getRelId(relName);
-    if (retVal == E_RELOPEN) {
-      return E_RELOPEN;
-    }
-    
-    // Call BlockAccess::renameAttribute with appropriate arguments.
+  // if the relation is open
+      //    (check if OpenRelTable::getRelId() returns E_RELNOTOPEN)
+      //    return E_RELOPEN
+  int retVal = OpenRelTable::getRelId(relName);
+  if (retVal == E_RELOPEN) {
+    return E_RELOPEN;
+  }
 
-    // return the value returned by the above renameAttribute() call
-    return BlockAccess::renameAttribute(relName, oldAttrName, newAttrName);
+  // Call BlockAccess::renameAttribute with appropriate arguments.
+
+  // return the value returned by the above renameAttribute() call
+  return BlockAccess::renameAttribute(relName, oldAttrName, newAttrName);
 }
