db470306b4d16dd8b8d5d82788c7b1a74cb37450
vikram18rock <v16ash@gmail.com>
2023-09-24 12:10:45 +0530

    stage8's bug modified:   ../mynitcbase/BlockAccess/BlockAccess.cpp

--- mynitcbase/BlockAccess/BlockAccess.cpp
+++ mynitcbase/BlockAccess/BlockAccess.cpp
@@ -609,6 +609,7 @@ int BlockAccess::deleteRelation(char relName[ATTR_SIZE]) {
     ***/
 
     // reset the searchIndex of the attribute catalog
+    RelCacheTable::resetSearchIndex(ATTRCAT_RELID);
 
     int numberOfAttributesDeleted = 0;
 
